bb97bc045e8efe19f214217fe1c4fd579f9eaa71
Mezosky <ignacio.meza@ug.uchile.cl>
2022-12-06 00:07:36 -0300

    json update

--- get_features2.py
+++ get_features2.py
@@ -199,8 +199,7 @@ def test(cfg):
     print("Rejected Videos: {}".format(rejected_vids))
     
     os.makedirs(out_path_metadata, exist_ok=True)
-    os.path.join(out_path_metadata, 'metadata_videos.json')
-    with open(out_path_metadata+'/metadata_videos.json', 'w') as f:
+    with open(os.path.join(out_path_metadata, 'metadata_videos.json'), 'w') as f:
         json.dump(metadata_json_file, f, indent=2)
         print("New json file was created to save the metadata.")
 
