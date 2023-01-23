fa238aecd8327995eca3054af5937444a25dbc43
Mezosky <ignacio.meza@ug.uchile.cl>
2023-01-22 21:44:14 -0300

    updating repo

--- utils/annotations_anet.py
+++ utils/annotations_anet.py
@@ -9,6 +9,8 @@ import json
 import numpy as np
 from tqdm import tqdm
 
+import ipdb
+
 
 def get_annotations(path1: str, path2: str, path3: str) -> None:
 
@@ -33,26 +35,30 @@ def get_annotations(path1: str, path2: str, path3: str) -> None:
     for it, ann_file in enumerate(list_annotations_files):
         new_json = []
         for e_file in tqdm(ann_file):
-            video_name = "v_" + e_file["video"]
-            fps = metadata_video[video_name]["fps"]
-            frames = int(metadata_video[video_name]["frames"])
+            try:
+                video_name = "v_" + e_file["video"]
+                fps = metadata_video[video_name]["fps"]
+                frames = int(metadata_video[video_name]["frames"])
 
-            fpath = os.path.join(path2, f"{video_name}.npy")
-            n_features = np.load(fpath).shape[0]
+                fpath = os.path.join(path2, f"{video_name}.npy")
+                n_features = np.load(fpath).shape[0]
+                
+                e_file["frame_start"] = e_file["time_start"] * fps
+                e_file["frame_end"] = e_file["time_end"] * fps
+                e_file["feature_start"] = e_file["frame_start"] / (
+                    frames / n_features
+                )
+                e_file["feature_end"] = e_file["frame_end"] / (frames / n_features)
+                e_file["number_features"] = n_features
+                e_file["number_frames"] = frames
+                e_file["fps"] = fps
+                e_file["preprocessing"] = "imeza_v0"
+                e_file["model"] = model_name
 
-            e_file["frame_start"] = e_file["time_start"] * fps
-            e_file["frame_end"] = e_file["time_end"] * fps
-            e_file["feature_start"] = e_file["frame_start"] / (
-                frames / n_features
-            )
-            e_file["feature_end"] = e_file["frame_end"] / (frames / n_features)
-            e_file["number_features"] = n_features
-            e_file["number_frames"] = frames
-            e_file["fps"] = fps
-            e_file["preprocessing"] = "imeza_v0"
-            e_file["model"] = model_name
-
-            new_json.append(e_file)
+                new_json.append(e_file)
+            except Exception as e:
+                print(e)
+                pass
 
         if it == 0:
             new_json_name = f"anet_train_tokens_{model_name}.json"
